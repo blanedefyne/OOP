34483a2b34ebabb661a3722b8886ec76b7d4691f
blanedefyne <murdock8@mail.ru>
2023-10-02 10:36:23 +0700

    sunshine - noMoreCodeStyleBugs?

--- Task_1_1_2/src/test/java/ru/nsu/zelenin/PolynomialTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/zelenin/PolynomialTest.java
@@ -1,9 +1,8 @@
 package ru.nsu.zelenin;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 /**
  * Testing polynomial functions.
@@ -35,20 +34,23 @@ public class PolynomialTest {
     @Test
     public void takePrettyBigDerivative() {
         Polynomial p1 = new Polynomial(new double[]{7.72, 0, -25, 7, 2, 2.2, -9.1, 8});
-        Assertions.assertEquals(new Polynomial(new double[]{0}), p1.differentiate(10));
+        Assertions.assertEquals(new Polynomial(new double[]{0}),
+                p1.differentiate(10));
     }
 
     @Test
     public void takeAnotherBigDerivative() {
         Polynomial p1 = new Polynomial(new double[]{7.72, 0, -25, 7, 2, 2.2, -9.1, 8});
-        Assertions.assertEquals(new Polynomial(new double[]{264, -6552, 20160}), p1.differentiate(5));
+        Assertions.assertEquals(new Polynomial(new double[]{264, -6552, 20160}),
+                p1.differentiate(5));
     }
 
     @Test
     public void exampleFirstTest() {
         Polynomial p1 = new Polynomial(new double[]{4, 3, 6, 7});
         Polynomial p2 = new Polynomial(new double[]{3, 2, 8});
-        Assertions.assertEquals("7.0x^3 + 6.0x^2 + 19.0x + 6.0", p1.plus(p2.differentiate(1)).toString());
+        Assertions.assertEquals("7.0x^3 + 6.0x^2 + 19.0x + 6.0",
+                p1.plus(p2.differentiate(1)).toString());
     }
 
     @Test
@@ -63,7 +65,8 @@ public class PolynomialTest {
         Polynomial p1 = new Polynomial(new double[]{5, -9, 14, -14, 21, 0, 0, 12, 11});
         Polynomial p2 = new Polynomial(new double[]{-3, 0, -36, 140, 1, -9});
         Assertions.assertEquals(new Polynomial(new double[]
-                        {-15, 27, -222, 1066, -1822, 2410, -2621, 2764, 114, -621, 1284, 1552, -97, -99}),
+                        {-15, 27, -222, 1066, -1822, 2410, -2621,
+                                2764, 114, -621, 1284, 1552, -97, -99}),
                 p1.times(p2));
     }
 
@@ -72,7 +75,7 @@ public class PolynomialTest {
         Polynomial p1 = new Polynomial(new double[]{24, 9, -2, -3, 4, 0, 13, 0, -29, 8});
         Assertions.assertEquals(
                 "8.0x^9 - 29.0x^8 + 576.0x^7 - 1611.0x^6 "
-                         + "+ 394.0x^4 - 3.0x^3 + 46.0x^2 - 9.0x + 20.0",
+                        + "+ 394.0x^4 - 3.0x^3 + 46.0x^2 - 9.0x + 20.0",
                 p1.plus(p1.differentiate(2)).toString());
     }
 
