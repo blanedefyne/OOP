f8a98d85e67622db4f620b30b7d9366af28b2c88
Pavel Zelenin <murdock8@mail.ru>
2023-11-21 02:20:19 +0700

    15GB test added

--- Task_1_3_1/src/test/java/ru/nsu/zelenin/substring/SubstringFinderTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/zelenin/substring/SubstringFinderTest.java
@@ -2,6 +2,10 @@ package ru.nsu.zelenin.substring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -68,5 +72,54 @@ class SubstringFinderTest {
         assertEquals(new ArrayList<>(), finder.find("noSubstring.txt", "something"));
     }
 
+    /**
+     * 16106127360 == 15 gb in bytes.
+     * + 78 bytes we added before cycle == 16106127438
+     * by the end of cycle fileSizeInBytes == -2, so we added 2 extra bytes
+     * 16106127438 + 2 == 16106127440
+     * and then + substring.length (it's 13)
+     * and + 13
+     */
+    @Test
+    public void checkOn15Gb() {
+        File bigText = new File(getClass()
+                .getClassLoader()
+                .getResource("")
+                .getPath() + "/large.txt");;
+        long fileSizeInBytes = 15L * 1024 * 1024 * 1024;
+        SubstringFinder finder = new SubstringFinder(150);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(bigText))) {
+            String string = "american dragon jake long ";
+            writer.write(string);
+            writer.write("jessica jones");
+            writer.write(string);
+            writer.write("jessica jones");
+
+            while (fileSizeInBytes > 0) {
+
+                writer.write(string);
+
+                fileSizeInBytes -= string.length();
+            }
+            writer.write("jessica jones");
+            writer.write("jessica jones");
+            writer.write("jessica jones");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<Long> res = finder.find("large.txt", "jessica jones");
+        bigText.delete();
+
+        List<Long> expected = new ArrayList<>();
+        expected.add(26L);
+        expected.add(65L);
+        expected.add(16106127440L);
+        expected.add(16106127453L);
+        expected.add(16106127466L);
+
+        assertEquals(expected, res);
+    }
 
 }
