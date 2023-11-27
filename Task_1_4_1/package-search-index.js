d34f1b7a0c1f68255622e3ac069d9a1633b72070
blanedefyne <blanedefyne@users.noreply.github.com>
2023-11-27 23:27:01 +0000

    Deploying to gh-pages from @ blanedefyne/OOP@d864a76498f528a19f1e4aef81aa01a58400b0c2 🚀

--- Task_1_4_1/package-search-index.js
+++ Task_1_4_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.zelenin.reportCard"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.zelenin.reportcard"}];updateSearchResults();
