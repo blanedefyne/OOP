ccf55e9fe4a258d05f6465a19d8982d5a19a4391
blanedefyne <blanedefyne@users.noreply.github.com>
2024-02-21 12:14:20 +0000

    Deploying to gh-pages from @ blanedefyne/OOP@682115ed3bf1f7e5c750376135542e6427f1ee81 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.zelenin.composites","l":"ConcurrentComposites"},{"p":"ru.nsu.zelenin.composites","l":"ConcurrentStreams"},{"p":"ru.nsu.zelenin.composites","l":"ProgressivelyComposites"}]
